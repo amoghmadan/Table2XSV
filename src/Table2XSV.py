8ba671812ea17e64a3f6e5aca14969819b3e8dae
Amogh Madan <amoghmadaan@gmail.com>
2019-06-29 09:34:31 +0530

    Add files via upload

--- src/Table2XSV.py
+++ src/Table2XSV.py
@@ -1,5 +1,6 @@
-""" Command Line tool to handle XSV """
+""" Command Line tool to transform Table Data to XSV """
 import argparse
+from datetime import datetime
 import sys
 import MySQLdb
 import pandas as pd
@@ -7,9 +8,10 @@ import sqlite3
 
 
 class Table2XSV(object):
-    """ Class to convert Sheet Data to XSV. """
+    """ Class to convert Table Data to XSV. """
 
-    def __init__(self, sep, encoding):
+    def __init__(self, outfile, sep, encoding):
+        self.__outfile = outfile
         self.__sep = sep
         self.__index = False
         self.__encoding = encoding
@@ -23,14 +25,14 @@ class Table2XSV(object):
     def csv2xsv(self, **kwargs):
         if kwargs["path"].endswith(".csv"):
             _df = pd.read_csv(kwargs["path"])
-            _df.to_csv("{}.csv".format(kwargs["path"]), index=self.__index, sep=self.__sep, encoding=self.__encoding)
+            _df.to_csv(self.__outfile, index=self.__index, sep=self.__sep, encoding=self.__encoding)
         else:
             print("Invalid file, can only accept files ending with .csv")
 
     def excel2xsv(self, **kwargs):
         if kwargs["path"].endswith(".xlsx"):
             _df = pd.read_excel(kwargs["path"], sheet_name=kwargs["sheet"])
-            _df.to_csv("{}.csv".format(kwargs["path"]), index=self.__index, sep=self.__sep, encoding=self.__encoding)
+            _df.to_csv(self.__outfile, index=self.__index, sep=self.__sep, encoding=self.__encoding)
         else:
             print("Invalid file, can only accept files ending with .xlsx")
 
@@ -39,7 +41,7 @@ class Table2XSV(object):
             _connection = sqlite3.connect(kwargs["path"])
             _df = pd.read_sql_query(kwargs["query"], con=_connection)
             _connection.close()
-            _df.to_csv("{}.csv".format(kwargs["path"]), index=self.__index, sep=self.__sep, encoding=self.__encoding)
+            _df.to_csv(self.__outfile, index=self.__index, sep=self.__sep, encoding=self.__encoding)
         else:
             print("Invalid file, can only accept files ending with .db")
 
@@ -48,18 +50,20 @@ class Table2XSV(object):
                                       passwd=kwargs["password"], db=kwargs["db"])
         _df = pd.read_sql(kwargs["query"], con=_connection)
         _connection.close()
-        _df.to_csv("{}.csv".format(kwargs["file"]), index=self.__index, sep=self.__sep, encoding=self.__encoding)
+        _df.to_csv(self.__outfile, index=self.__index, sep=self.__sep, encoding=self.__encoding)
 
 
 if __name__ == "__main__":
     try:
         parser = argparse.ArgumentParser()
         parser.add_argument("params", nargs="+", help="table type to convert to XSV and respective config")
+        parser.add_argument("-o", "--outfile", default="output {}.csv".format(datetime.now()),
+                            help="provide name for the output file with extension as .csv")
         parser.add_argument("-e", "--encoding", default="utf-8", help="provide an encoding enclosed in quotes")
         parser.add_argument("-s", "--sep", default=",", help="provide a separator enclosed in quotes")
         args = parser.parse_args()
 
-        t2xsv = Table2XSV(args.sep, args.encoding)
+        t2xsv = Table2XSV(args.outfile, args.sep, args.encoding)
 
         if len(args.params) == 1:
             print("Please refer command line args help by using -h or --help or go through the manual")
@@ -82,9 +86,9 @@ if __name__ == "__main__":
             else:
                 print(else_print)
         elif args.params[0].lower() == "mysql":
-            if len(args.params) == 8:
+            if len(args.params) == 7:
                 t2xsv.mysql2xsv(host=args.params[1], port=args.params[2], user=args.params[3], password=args.params[4],
-                                db=args.params[5], query=args.params[6], file=args.params[7])
+                                db=args.params[5], query=args.params[6])
             else:
                 print(else_print)
         else:
