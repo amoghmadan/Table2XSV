d32eb81e7f2909cdba3f2707a37442098e3b792c
amogh.madan <amoghmadaan@gmail.com>
2021-12-17 14:27:18 +0530

    Update mysqlclient version to suppory py310

--- tests/tests.py
+++ tests/tests.py
@@ -3,68 +3,169 @@ import sys
 import unittest
 from pathlib import Path
 
-from pandas import DataFrame
-
 BASE_DIR = Path(__file__).parent.parent
 sys.path.append(os.path.abspath(BASE_DIR))
+
+try:
+    from table2xsv.bin import execute_from_command_line
+    from table2xsv.core import CommandError
+    from table2xsv.core.helpers import call_command
+    from table2xsv.management.commands.csv import Command
+    from table2xsv.utils.getter import get_name
+except ModuleNotFoundError:
+    raise ModuleNotFoundError("Append BASE_DIR to sys.path list")
+
+ASSETS_DIR = BASE_DIR / "assets"
 
 
 class TestTable2XSV(unittest.TestCase):
     """Test Table2XSV"""
 
-    ASSETS_DIR = BASE_DIR / "assets"
+    name = get_name()
 
-    def test_csv2df(self):
+    def test_empty_base_command(self):
+        """Test Empty Command"""
+
+        argv = [self.name]
+        self.assertEqual(execute_from_command_line(argv), None)
+
+    def test_empty_specific_command(self):
+        """Test Empty Command"""
+
+        argv = [self.name, "csv"]
+        with self.assertRaises(SystemExit):
+            execute_from_command_line(argv)
+
+    def test_unknown_command(self):
+        """Test Unknown Command"""
+
+        argv = [self.name, "unknown"]
+        with self.assertRaises(SystemExit):
+            execute_from_command_line(argv)
+
+    def test_version(self):
+        """Test Version"""
+
+        argv = [self.name, "--version"]
+        self.assertEqual(execute_from_command_line(argv), None)
+
+    def test_version_by_class(self):
+        """Test Help By Class"""
+
+        with self.assertRaises(SystemExit):
+            call_command(Command(), "--version", verbosity=0)
+
+    def test_version_by_command_name(self):
+        """Test Help By Command Name"""
+
+        with self.assertRaises(SystemExit):
+            call_command("csv", "--version", verbosity=0)
+
+    def test_help(self):
+        """Test Version"""
+
+        argv = [self.name, "--help"]
+        self.assertEqual(execute_from_command_line(argv), None)
+
+    def test_help_by_class(self):
+        """Test Help By Class"""
+
+        with self.assertRaises(SystemExit):
+            call_command(Command(), "--help", verbosity=0)
+
+    def test_possible_match(self):
+        """Test Command Error"""
+
+        argv = [self.name, "xsv"]
+        with self.assertRaises(SystemExit):
+            execute_from_command_line(argv)
+
+    def test_unknown_command_error(self):
+        """Test Unknown Command Error"""
+
+        with self.assertRaises(CommandError):
+            call_command("unknown", verbosity=0)
+
+    def test_unknown_args(self):
+        """Test Unknown Command Error"""
+
+        argv = [self.name, "csv", "path", "-l", "82"]
+        with self.assertRaises(SystemExit):
+            execute_from_command_line(argv)
+
+    def test_help_by_command_name(self):
+        """Test Help By Command Name"""
+
+        with self.assertRaises(SystemExit):
+            call_command("csv", "--help", verbosity=0)
+
+    def test_help_for_commands(self):
+        """Test Commands Flag"""
+
+        argv = [self.name, "help", "csv"]
+        self.assertEqual(execute_from_command_line(argv), None)
+
+    def test_commands_flag(self):
+        """Test Commands Flag"""
+
+        argv = [self.name, "help", "--commands"]
+        self.assertEqual(execute_from_command_line(argv), None)
+
+    def test_csv_command(self):
         """Test CSV"""
 
-        from table2xsv.contrib.root.management.commands.csv import Command
+        options = {"path": os.path.abspath(ASSETS_DIR / "input.csv")}
+        argv = [self.name, "csv", options["path"]]
+        self.assertEqual(execute_from_command_line(argv), None)
 
-        kwargs = {"path": self.ASSETS_DIR / "input.csv"}
-        command = Command()
-        result = command.process_to_df(**kwargs)
-        self.assertTrue(isinstance(result, DataFrame))
-
-    def test_excel2df(self):
+    def test_excel_command(self):
         """Test Excel"""
 
-        from table2xsv.contrib.root.management import Command
-
-        kwargs = {"path": self.ASSETS_DIR / "input.xlsx", "sheet": "Sheet1"}
-        command = Command()
-        result = command.process_to_df(**kwargs)
-        self.assertTrue(isinstance(result, DataFrame))
+        options = {
+            "path": os.path.abspath(ASSETS_DIR / "input.xlsx"),
+            "sheet": "Sheet1",
+        }
+        argv = [self.name, "excel", options["path"], "-S", options["sheet"]]
+        self.assertEqual(execute_from_command_line(argv), None)
 
     @unittest.skip("MySQL, Switch Test Credentials")
-    def test_mysql2df(self):
+    def test_mysql_command(self):
         """Test MySQL"""
 
-        from table2xsv.contrib.root.management.commands.mysql import Command
-
-        kwargs = {"user": "root", "password": "root", "query": "SELECT 1 AS One;"}
-        command = Command()
-        result = command.process_to_df(**kwargs)
-        self.assertTrue(isinstance(result, DataFrame))
+        options = {"user": "root", "password": "toor", "query": "SELECT 1 AS One;"}
+        argv = [
+            self.name,
+            "mysql",
+            options["user"],
+            options["password"],
+            options["query"],
+        ]
+        self.assertEqual(execute_from_command_line(argv), None)
 
     @unittest.skip("Neo4j, Switch Test Credentials")
-    def test_neo4j2df(self):
+    def test_neo4j_command(self):
         """Test Neo4j"""
 
-        from table2xsv.contrib.root.management import Command
+        options = {
+            "user": "neo4j",
+            "password": "neo4j",
+            "query": "MATCH 1 AS One;",
+        }
+        argv = [
+            self.name,
+            "neo4j",
+            options["user"],
+            options["password"],
+            options["query"],
+        ]
+        self.assertEqual(execute_from_command_line(argv), None)
 
-        kwargs = {"user": "neo4j", "password": "neo4j", "query": "MATCH 1 AS One;"}
-        command = Command()
-        result = command.process_to_df(**kwargs)
-        self.assertTrue(isinstance(result, DataFrame))
-
-    def test_sqlite2df(self):
+    def test_sqlite_command(self):
         """Test SQLite"""
 
-        from table2xsv.contrib.root.management.commands.sqlite import Command
-
-        kwargs: dict = {
-            "path": self.ASSETS_DIR / "input.sqlite3",
+        options = {
+            "path": os.path.abspath(ASSETS_DIR / "input.sqlite3"),
             "query": "SELECT id FROM One;",
         }
-        command = Command()
-        result = command.process_to_df(**kwargs)
-        self.assertTrue(isinstance(result, DataFrame))
+        argv = [self.name, "sqlite", options["path"], options["query"]]
+        self.assertEqual(execute_from_command_line(argv), None)
