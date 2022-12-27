0cb242da3adac53b225d6243dc6b3a820fe75ebc
Amogh Madan <amogh.madan@acsicorp.com>
2022-12-27 16:28:43 +0530

    Fix mypy issues

--- src/table2xsv/utils/getter.py
+++ src/table2xsv/utils/getter.py
@@ -1,28 +1,24 @@
 from pathlib import Path
 
-from table2xsv import __name__, __version__
+import table2xsv
 
 
 def get_package_path():
     """Get Package Path"""
-
     package_path = Path(__file__).parent.parent
     return package_path
 
 
 def get_name():
     """Get Name"""
-
-    return __name__
+    return table2xsv.__name__
 
 
 def get_version():
     """Get Version"""
-
-    return __version__
+    return table2xsv.__version__
 
 
 def get_named_version():
     """Get Named Version"""
-
     return get_name() + " " + get_version()
